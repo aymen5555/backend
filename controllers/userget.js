8c719fbf32d2a3bd377d09d5665c89c8d9a36746
Fedi <jerbifedi@isimsf.u-sfax.tn>
2026-04-03 21:19:26 +0100

    espaceadmin:fix

--- controllers/userget.js
+++ controllers/userget.js
@@ -1,15 +1,20 @@
 const db =require("../Database") ;
 exports.getUser = async (req,res) => { 
+    try {
     const role = req.user.role ; 
     if (role !== 'admin') { 
         return res.status(403).json({message:"forbidden"}) ; 
-    } 
+    }  
+
     const query = 'SELECT id,created_at,name,email,role,cin,plan FROM users' ; 
     db.query(query, (err, result) => { 
         if (err) { 
             return res.status(500).json({message:"error"}) ; 
         } 
         res.json(result.rows) ; 
-    }) ; 
+    }) ;  
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
 
 }
