f65fa6f9e7a49b745979df6849cc89772fb389e2
Fedi <jerbifedi@isimsf.u-sfax.tn>
2026-04-28 17:12:31 +0100

    change stat

--- Route.js
+++ Route.js
@@ -4,12 +4,16 @@ const authController = require('./controllers/authController');
 const authMiddleware = require('./middleware/middleware').verifToken;
 const ChangeController = require('./controllers/Change_controller');
 const createpatientController = require('./controllers/PatientCon');
+const Changepassword = require ('./controllers/forgotpassword') ;
 
 router.post('/register-patient', createpatientController.registerPatient);
 router.post('/login', authController.login);
 router.post('/change-password', authMiddleware, ChangeController.changePassword);
+router.post('/forgot-password', Changepassword.forgotPassword);
+router.post('/verify-code', Changepassword.verifyCode);
 router.get('/medecin', authMiddleware, require('./controllers/getmedecin').getmedecin);
 router.get('/users', authMiddleware, require('./controllers/userget').getUser);
+router.get('/users/stats', authMiddleware, require('./controllers/userget').getUserStats);
 
 module.exports = router;
 
